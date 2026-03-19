b89054aaaecf7eb7f8ec5a328bc8f4bd619f4b6d
purvjoshi <101319136+purvjoshi04@users.noreply.github.com>
2026-03-19 23:04:35 +0530

    added prometheus and grafana code

--- week-26-prom/src/index.ts
+++ week-26-prom/src/index.ts
@@ -1,21 +1,16 @@
-import client  from 'prom-client';
 import express from "express";
-import { metricsMiddleware} from "./metrics/middleware";
+import { middleware } from "./middleware";
+import client from "prom-client";
+import { metricsMiddleware } from "./metrics";
 
 const app = express();
-app.use(metricsMiddleware)
 
-app.get("/cpu", (req, res) => {
-    for (let i = 0; i < 1000000; i++) {
-        Math.random();
-    }
-
-    res.json({
-        message: "cpu"
-    })
-});
+app.use(express.json());
+app.use(middleware);
+app.use(metricsMiddleware);
 
 app.get("/user", async (req, res) => {
+
     await new Promise((resolve) => setTimeout(resolve, 1000));
     res.send({
         name: "John Doe",
@@ -23,11 +18,18 @@ app.get("/user", async (req, res) => {
     });
 });
 
+app.post("/user", (req, res) => {
+    const user = req.body;
+    res.send({
+        ...user,
+        id: 1,
+    });
+});
+
 app.get("/metrics", async (req, res) => {
     const metrics = await client.register.metrics();
     res.set('Content-Type', client.register.contentType);
     res.end(metrics);
-});
+})
 
-
-app.listen(5001);
+app.listen(5002);
--- week-31/middleware.ts
+++
@@ -1,8 +0,0 @@
-import type { NextFunction, Request, Response } from "express";
-
-export const middleware = (req: Request, res: Response, next: NextFunction) => {
-    const statTime = Date.now();
-    next();
-    const endTime = Date.now();
-    console.log(`Time it took is ${endTime - statTime} ms for method ${req.method} for route ${(req.originalUrl)}`);
-}
