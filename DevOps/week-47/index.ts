f1f33aa31f30ffa63229ce8b5aaf676e2d3bd6a6
purvjoshi <101319136+purvjoshi04@users.noreply.github.com>
2026-07-03 22:54:30 +0530

    cloudflare turnstile example

--- DevOps/week-47/index.ts
+++ DevOps/week-47/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({});
 
 const app = express();
 app.use(express.json());
@@ -19,8 +22,27 @@ app.post("/generate-otp", (req, res) => {
     });
 });
 
-app.post("/verify-otp", (req, res) => {
-    const {email, otp, newPassword} = req.body;
+app.post("/verify-otp", async (req, res) => {
+    const {email, otp, newPassword, token} = req.body;
+
+    let formData = new FormData();
+    formData.append('secret', process.env.SECRET_KEY!);
+    formData.append('response', token);
+
+    const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+
+    const result = await fetch(url, {
+        body: formData,
+        method: 'POST'
+    });
+
+    const challengeSucceeded = ((await result.json()) as { success: boolean }).success;
+
+    if (!challengeSucceeded) {
+        return res.status(403).json({
+            message: "You are a bot"
+        });
+    }
 
     if(validOtps[email] == otp) {
         console.log(`Password reset for user ${email} with ${newPassword}`);
